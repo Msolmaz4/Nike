c4018aec8850d203e7cbd2bfb59f53fbeefc2258
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-10-20 10:18:39 +0200

    anfag

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Hero} from './components'
+
+const App = () => {
+  return (
+    <div>
+    <main>
+      <Hero/>
+    </main>
+
+    </div>
+  )
+}
+
+export default App
